bd6a7606a7b9c1eff4c376d69e642f4a8233bce7
[dyad] <git@dyad.sh>
2026-04-20 13:43:21 +0200

    Updated public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,42 +1,30 @@
-const CACHE_NAME = "coop-keeper-v1";
-
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
-];
+const CACHE_NAME = "coop-keeper-v2";
 
 // INSTALL
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  self.skipWaiting();
 });
 
 // ACTIVATE
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((names) =>
-      Promise.all(
-        names.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      )
-    )
-  );
+  event.waitUntil(self.clients.claim());
 });
 
-// FETCH
+// FETCH (CACHE FIRST)
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request)
-    )
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request)
+          .then((res) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, res.clone());
+              return res;
+            });
+          })
+          .catch(() => caches.match("/index.html"))
+      );
+    })
   );
 });
