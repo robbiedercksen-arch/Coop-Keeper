07cd2fef92e0562ec63b15bf86f25c05d98fe7fe
[dyad] <git@dyad.sh>
2026-04-22 14:07:29 +0200

    Updated src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-// Register Service Worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(() => console.log("Service Worker registered"))
-      .catch((err) => console.log("SW registration failed:", err));
-  });
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
