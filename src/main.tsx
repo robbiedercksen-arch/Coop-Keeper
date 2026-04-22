ce6e895907c5a2d90973994ee3ee0545492a5627
[dyad] <git@dyad.sh>
2026-04-22 13:31:34 +0200

    Updated src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Register Service Worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(() => console.log("Service Worker registered"))
-      .catch((err) => console.log("SW registration failed:", err));
-  });
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
