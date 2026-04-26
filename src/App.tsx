beba0e6fc0218f96e0b620745693ade1881b980a
[dyad] <git@dyad.sh>
2026-04-26 12:35:20 +0200

    Updated src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import ChickenRegistry from "./pages/ChickenRegistry";
+
+export default function App() {
+  const [activePage, setActivePage] = useState("dashboard");
+
+  const renderPage = () => {
+    switch (activePage) {
+      case "registry":
+        return <ChickenRegistry />;
+      default:
+        return <Dashboard />;
+    }
+  };
+
+  return (
+    <div className="flex">
+      <Sidebar setActivePage={setActivePage} />
+      <div className="flex-1 bg-[#f5f1ea] min-h-screen">
+        {renderPage()}
+      </div>
+    </div>
+  );
+}
