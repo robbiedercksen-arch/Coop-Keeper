1f599f81b8df8850fb2b50f167eda7b4c20537cd
[dyad] <git@dyad.sh>
2026-04-26 11:31:06 +0200

    Updated src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import "./index.css";   // MUST BE FIRST
-import "./globals.css"; // MUST BE SECOND
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./globals.css"; // ONLY this
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <App />
