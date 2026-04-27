f8bc385fccb296f6b4da446892e48529fa778a9e
[dyad] <git@dyad.sh>
2026-04-27 15:57:25 +0200

    Updated src/pages/ChickenRegistry.tsx

--- src/pages/ChickenRegistry.tsx
+++
@@ -1,9 +0,0 @@
-<span style={{ fontSize: 18 }}>
-  {(!chicken.healthLogs || chicken.healthLogs.length === 0)
-    ? "🟢"
-    : chicken.healthLogs.some((l: any) => !l.resolved && l.status === "Sick")
-    ? "🔴"
-    : chicken.healthLogs.some((l: any) => !l.resolved && l.status === "Recovering")
-    ? "🟡"
-    : "🟢"}
-</span>
