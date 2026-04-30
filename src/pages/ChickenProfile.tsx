a99c7f733d8200feae8011be2f1a61a716360e10
[dyad] <git@dyad.sh>
2026-04-30 14:23:06 +0200

    Updated src/pages/ChickenProfile.tsx

--- src/pages/ChickenProfile.tsx
+++ src/pages/ChickenProfile.tsx
@@ -388,10 +388,11 @@ const header = {
           style={{ marginLeft: 8 }}
         />
       </label>
+</label>
+</div>   {/* ✅ CLOSE checkbox container FIRST */}
+
 <div style={{ marginTop: 6 }}>
   <button onClick={() => setViewLog(log)}>View</button>
   <button onClick={() => editHealthLog(log)}>Edit</button>
   <button onClick={() => deleteHealthLog(log.id)}>Delete</button>
 </div>
-</div>
-))}
