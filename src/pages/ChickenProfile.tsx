669b3fd9ecff0c819c92c679ca53bfbff11c95bd
[dyad] <git@dyad.sh>
2026-04-30 18:22:51 +0200

    Updated src/pages/ChickenProfile.tsx

--- src/pages/ChickenProfile.tsx
+++ src/pages/ChickenProfile.tsx
@@ -137,6 +137,23 @@ const saveChickenInfo = () => {
 };
 
   const input = {
+   const [editingChicken, setEditingChicken] = useState(false);
+
+const [editForm, setEditForm] = useState({
+  name: chicken.name || "",
+  idTag: chicken.idTag || "",
+  breed: chicken.breed || "",
+  sex: chicken.sex || "Hen",
+  ageGroup: chicken.ageGroup || "",
+});
+
+const saveChickenInfo = () => {
+  updateChicken({
+    ...chicken,
+    ...editForm,
+  });
+  setEditingChicken(false);
+};
     display: "block",
     width: "100%",
     marginBottom: 10,
