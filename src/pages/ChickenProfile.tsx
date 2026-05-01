0019c44cde9845131e15919c1100b2348df1832e
[dyad] <git@dyad.sh>
2026-05-01 10:23:36 +0200

    Updated src/pages/ChickenProfile.tsx

--- src/pages/ChickenProfile.tsx
+++ src/pages/ChickenProfile.tsx
@@ -308,16 +308,55 @@ export default function ChickenProfile({
         </label>
 
         <div style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
-          gap: 8,
-          marginTop: 10
-        }}>
-          {(chicken.album || []).map((img: any, i: number) => (
-            <img key={i} src={img} style={{ width: "100%", borderRadius: 10 }} />
-          ))}
-        </div>
-      </div>
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gap: 8,
+  marginTop: 10
+}}>
+  {(chicken.album || []).map((img: any, i: number) => (
+    <div key={i} style={{ position: "relative" }}>
+
+      <img
+        src={img}
+        onClick={() => setActiveImage(img)}
+        style={{
+          width: "100%",
+          aspectRatio: "1 / 1",
+          objectFit: "cover",
+          borderRadius: 10,
+          cursor: "pointer"
+        }}
+      />
+
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          updateChicken({
+            ...chicken,
+            album: chicken.album.filter((_: any, index: number) => index !== i),
+          });
+        }}
+        style={{
+          position: "absolute",
+          top: 6,
+          right: 6,
+          background: "rgba(0,0,0,0.6)",
+          color: "#fff",
+          border: "none",
+          borderRadius: "50%",
+          width: 22,
+          height: 22,
+          fontSize: 12,
+          cursor: "pointer",
+        }}
+      >
+        ✕
+      </button>
+
+    </div>
+  ))}
+</div>
+</div>
 {/* HEALTH LOGS */}
 <div style={card}>
   <div style={sectionTitle}>🩺 Health Logs</div>
