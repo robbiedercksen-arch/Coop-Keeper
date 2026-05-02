bc6d897334bc555d9b3bb5ff74557f1b84d16a99
[dyad] <git@dyad.sh>
2026-05-02 12:14:51 +0200

    Updated src/pages/ChickenProfile.tsx

--- src/pages/ChickenProfile.tsx
+++ src/pages/ChickenProfile.tsx
@@ -535,54 +535,70 @@ return (
         <div style={{
   display: "grid",
   gridTemplateColumns: "repeat(3, 1fr)",
-  gap: 8,
-  marginTop: 10
+  gap: 10,
+  marginTop: 12
 }}>
   {(chicken.album || []).map((img: any, i: number) => (
-  <div key={i} style={{ position: "relative" }}>
-
-    <img
-      src={img}
-      onClick={() => {
-        setActiveIndex(i);
-        setActiveImage(img);
-      }}
+    <div
+      key={i}
       style={{
-        width: "100%",
-        aspectRatio: "1 / 1",
-        objectFit: "cover",
-        borderRadius: 10,
-        cursor: "pointer"
-      }}
-    />
-
-    <button
-      onClick={(e) => {
-        e.stopPropagation();
-        updateChicken({
-          ...chicken,
-          album: chicken.album.filter((_: any, index: number) => index !== i),
-        });
-      }}
-      style={{
-        position: "absolute",
-        top: 6,
-        right: 6,
-        background: "rgba(0,0,0,0.6)",
-        color: "#fff",
-        border: "none",
-        borderRadius: "50%",
-        width: 22,
-        height: 22,
-        fontSize: 12,
-        cursor: "pointer",
+        position: "relative",
+        borderRadius: 14,
+        overflow: "hidden",
       }}
     >
-      ✕
-    </button>
 
-  </div>
-))}
+      {/* IMAGE */}
+      <img
+        src={img}
+        onClick={() => {
+          setActiveIndex(i);
+          setActiveImage(img);
+        }}
+        style={{
+          width: "100%",
+          aspectRatio: "1 / 1",
+          objectFit: "cover",
+          cursor: "pointer",
+          transition: "transform 0.2s ease"
+        }}
+        onMouseDown={(e) => (e.currentTarget.style.transform = "scale(0.96)")}
+        onMouseUp={(e) => (e.currentTarget.style.transform = "scale(1)")}
+        onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
+      />
+
+      {/* DELETE BUTTON */}
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          updateChicken({
+            ...chicken,
+            album: chicken.album.filter((_: any, index: number) => index !== i),
+          });
+        }}
+        style={{
+          position: "absolute",
+          top: 6,
+          right: 6,
+          background: "rgba(0,0,0,0.6)",
+          backdropFilter: "blur(6px)",
+          color: "#fff",
+          border: "none",
+          borderRadius: "50%",
+          width: 26,
+          height: 26,
+          fontSize: 14,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          cursor: "pointer",
+        }}
+      >
+        ✕
+      </button>
+
+    </div>
+  ))}
 </div>
 {/* IMAGE VIEWER */}
 {activeImage && (
