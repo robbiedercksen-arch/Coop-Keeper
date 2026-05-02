a2b10040aa67dc900af01d9379faaadfc46ed4c6
[dyad] <git@dyad.sh>
2026-05-02 10:59:25 +0200

    Updated src/pages/ChickenProfile.tsx

--- src/pages/ChickenProfile.tsx
+++ src/pages/ChickenProfile.tsx
@@ -500,7 +500,6 @@ return (
   gap: 8,
   marginTop: 10
 }}>
-
   {(chicken.album || []).map((img: any, i: number) => (
     <div key={i} style={{ position: "relative" }}>
 
@@ -543,7 +542,24 @@ return (
       >
         ✕
       </button>
-
+{/* IMAGE VIEWER */}
+{activeImage && (
+  <div
+    onClick={() => setActiveImage(null)}
+    onTouchStart={handleTouchStart}
+    onTouchEnd={handleTouchEnd}
+    style={{
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100vw",
+      height: "100vh",
+      background: "rgba(0,0,0,0.95)",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      zIndex: 999999,
+    }}
   >
     <img
       src={activeImage}
