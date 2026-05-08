e1aac8a5ef445688d97ed5592e359dc0ee529f62
robbiedercksen-arch <robbiedercksen@gmail.com>
2026-05-08 14:39:10 +0200

    Update IncubatorRegistry.tsx

---
+++ src/pages/IncubatorRegistry.tsx
@@ -0,0 +1,14 @@
+export default function IncubatorRegistry() {
+  return (
+    <div
+      style={{
+        background: "#fff",
+        padding: 20,
+        borderRadius: 12,
+      }}
+    >
+      <h2>🐣 Incubator Registry</h2>
+      <p>Incubator system coming soon...</p>
+    </div>
+  );
+}
